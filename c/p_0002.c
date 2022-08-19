e5e1b95dba543590b5e914b5b1b109d8409776a2
Pietro F. Maggi <pfm@pietromaggi.com>
2022-08-19 17:50:29 +0200

    Add problem 1 and 2 solutions in C.

---
+++ c/p_0002.c
@@ -0,0 +1,29 @@
+/*
+ * By considering the terms in the Fibonacci sequence whose values do no
+ * exceed four million, find the sum of the even-valued terms.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+
+int main(void) {
+
+    u_int64_t first = 1;
+    u_int64_t second = 2;
+    u_int64_t sum = 0;
+    u_int64_t swap = 0;
+
+    while (second < 4000000) {
+        if (second % 2 == 0) {
+            sum += second;
+        }
+        swap = first;
+        first = second;
+        second = swap + second;
+    }
+
+    printf("Result = %lu\n", sum);
+
+    return EXIT_SUCCESS;
+}
