d6ba31e7a5cc48b34fa61b247f636757d749966e
Pietro F. Maggi <pfm@pietromaggi.com>
2024-03-26 22:02:42 +0100

    Add problem 2 in C++

---
+++ cpp/p_0002.cc
@@ -0,0 +1,24 @@
+/*
+ * By considering the terms in the Fibonacci sequence whose values do not
+ * exceed four million, find the sum of the even-valued terms.
+ */
+
+#include <cstdio>
+
+int sum_even_fibonacci(int limit) {
+    // Return the sum of the even-valued terms.
+    int total{};
+    int first{1};
+    int second{2};
+    while (second < limit) {
+        if (second % 2 == 0) total += second;
+        int temp = second;
+        second += first;
+        first = temp;
+    }
+    return total;
+}
+
+int main() {
+    printf("Value: %d\n", sum_even_fibonacci(4000000));
+}
