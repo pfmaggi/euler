3a81049298a9820c88288295be827d967c3b8fb7
Pietro F. Maggi <pfm@pietromaggi.com>
2024-05-10 21:57:33 +0200

    Add problem 3 in C++

---
+++ cpp/p_0003.cc
@@ -0,0 +1,27 @@
+/*
+ * What is the largest prime factor of the number 600851475143?
+ */
+
+#include <cstdio>
+
+unsigned long findLargestPrimeFactor(unsigned long number) {
+    auto result{0};
+    while (number % 2 == 0) {
+        result = 2;
+        number /= 2;
+    }
+    for (auto i{3}; i < number; i += 2) {
+        while (number % i == 0) {
+            result = i;
+            number /= i;
+        }
+    }
+
+    if (number > 2) result = number;
+
+    return result;
+}
+
+int main() {
+    printf("Result = %lu\n", findLargestPrimeFactor(600851475143));
+}
