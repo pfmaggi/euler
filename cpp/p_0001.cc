7a229e2eaa927972f1fca6b69732265cdd9d6e43
Pietro F. Maggi <pfm@pietromaggi.com>
2025-01-10 21:39:11 +0100

    Refactor problem 1 in C++

--- cpp/p_0001.cc
+++ cpp/p_0001.cc
@@ -1,18 +1,18 @@
 /* Find the sum of all the multiples of 3 and 5 below 1000 */
 
 #include <iostream>
+#include <numeric>
 #include <ranges>
 
 auto p_0001(int max) {
-    int sum{};
-
-    for (int i : std::ranges::iota_view{1, max}) {
-        if ((i % 3 == 0) || (i % 5 == 0)) {
-            sum += i;
-        }
-    }
-
-    return sum;
+    auto numbers = std::ranges::iota_view{1, max};
+    return std::accumulate(numbers.begin(), numbers.end(), 0,
+                           [](int sum, int x) {
+                               if ((x % 3 == 0) || (x % 5 == 0)) {
+                                   sum += x;
+                               }
+                               return sum;
+                           });
 }
 
 int main(void) { std::cout << "Result = " << p_0001(1000) << '\n'; }
