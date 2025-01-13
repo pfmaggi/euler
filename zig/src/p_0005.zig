ad02855aa34a4cccbbb9e2402b3b7f256fe0b474
Pietro F. Maggi <pfm@pietromaggi.com>
2025-01-13 20:56:49 +0100

    Generalize problem 5 in Zig

--- zig/src/p_0005.zig
+++ zig/src/p_0005.zig
@@ -3,24 +3,70 @@
 // What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
 const std = @import("std");
-const fmt = std.fmt;
 
-// The value is computed multiplying the primes below upper bound at their
-// maximum elevation available below upper bound. For example:
-// 10: 2^3 and 3^2
-// 20: 2^4 and 3^2
-fn compute(upper_bound: u8) u64 {
-    if (upper_bound == 10) return (5 * 7 * 8 * 9);
-    if (upper_bound == 20) return (5 * 7 * 9 * 11 * 13 * 16 * 17 * 19);
-    return 0;
+fn computeFactors(num: u32, allocator: std.mem.Allocator) !std.AutoHashMap(u32, u32) {
+    var remainder = num;
+    var factors = std.AutoHashMap(u32, u32).init(allocator);
+
+    var i: u32 = 2;
+    while (i <= num) : (i += 1) {
+        while (remainder % i == 0) {
+            remainder = remainder / i;
+            const gop = try factors.getOrPut(i);
+            if (gop.found_existing) {
+                gop.value_ptr.* += 1;
+            } else {
+                gop.value_ptr.* = 1;
+            }
+        }
+    }
+    return factors;
 }
 
-pub fn main() anyerror!void {
-    const stdout = std.io.getStdOut().writer();
+fn commonFactors(limit: u32, allocator: std.mem.Allocator) !u32 {
+    var factors = std.AutoHashMap(u32, u32).init(allocator);
+    defer factors.deinit();
 
-    try stdout.print("Problem 0005: The smallest positive number that is evenly divisible by all numbers from 1 to 20 is: {d}\n", .{compute(20)});
+    var i: u32 = 2;
+    while (i <= limit) : (i += 1) {
+        var num_factors = try computeFactors(i, allocator);
+        defer num_factors.deinit(); // Deallocate num_factors after each iteration
+        var iterator = num_factors.iterator();
+        while (iterator.next()) |entry| {
+            const gop = try factors.getOrPut(entry.key_ptr.*);
+            if (gop.found_existing) {
+                if (gop.value_ptr.* < entry.value_ptr.*) {
+                    gop.value_ptr.* = entry.value_ptr.*;
+                }
+            } else {
+                gop.value_ptr.* = entry.value_ptr.*;
+            }
+        }
+    }
+
+    var iterator = factors.iterator();
+    var result: u32 = 1;
+    while (iterator.next()) |entry| {
+        result *= std.math.pow(u32, entry.key_ptr.*, entry.value_ptr.*);
+    }
+
+    return result;
+}
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    const stdout = std.io.getStdOut().writer();
+    try stdout.print("{}\n", .{try commonFactors(20, allocator)});
 }
 
 test "2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder" {
-    try std.testing.expect(compute(10) == 2520);
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    const result: u32 = try commonFactors(10, allocator);
+    try std.testing.expect(result == 2520);
 }
