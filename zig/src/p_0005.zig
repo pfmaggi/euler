3485403e4774f917a4e43af961c1a7f07a896172
Pietro F. Maggi <pfm@pietromaggi.com>
2025-01-13 21:26:04 +0100

    Refactor problem 5 in Rust and Zig

--- zig/src/p_0005.zig
+++ zig/src/p_0005.zig
@@ -4,69 +4,29 @@
 
 const std = @import("std");
 
-fn computeFactors(num: u32, allocator: std.mem.Allocator) !std.AutoHashMap(u32, u32) {
-    var remainder = num;
-    var factors = std.AutoHashMap(u32, u32).init(allocator);
-
-    var i: u32 = 2;
-    while (i <= num) : (i += 1) {
-        while (remainder % i == 0) {
-            remainder = remainder / i;
-            const gop = try factors.getOrPut(i);
-            if (gop.found_existing) {
-                gop.value_ptr.* += 1;
-            } else {
-                gop.value_ptr.* = 1;
-            }
-        }
-    }
-    return factors;
+// Calculate greates common divisor (GCD)
+fn gcd(a: u64, b: u64) u64 {
+    if (b == 0) return a;
+    return gcd(b, a % b);
 }
 
-fn commonFactors(limit: u32, allocator: std.mem.Allocator) !u32 {
-    var factors = std.AutoHashMap(u32, u32).init(allocator);
-    defer factors.deinit();
+// Calculate least common multiple (LCM)
+fn lcm(a: u64, b: u64) u64 {
+    return a * b / gcd(a, b);
+}
 
-    var i: u32 = 2;
-    while (i <= limit) : (i += 1) {
-        var num_factors = try computeFactors(i, allocator);
-        defer num_factors.deinit(); // Deallocate num_factors after each iteration
-        var iterator = num_factors.iterator();
-        while (iterator.next()) |entry| {
-            const gop = try factors.getOrPut(entry.key_ptr.*);
-            if (gop.found_existing) {
-                if (gop.value_ptr.* < entry.value_ptr.*) {
-                    gop.value_ptr.* = entry.value_ptr.*;
-                }
-            } else {
-                gop.value_ptr.* = entry.value_ptr.*;
-            }
-        }
+fn compute(limit: u64) u64 {
+    var result: u64 = 1;
+    for (1..limit + 1) |i| {
+        result = lcm(result, i);
     }
-
-    var iterator = factors.iterator();
-    var result: u32 = 1;
-    while (iterator.next()) |entry| {
-        result *= std.math.pow(u32, entry.key_ptr.*, entry.value_ptr.*);
-    }
-
     return result;
 }
 
 pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
-    const allocator = gpa.allocator();
-
-    const stdout = std.io.getStdOut().writer();
-    try stdout.print("{}\n", .{try commonFactors(20, allocator)});
+    std.debug.print("{}\n", .{compute(20)});
 }
 
 test "2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder" {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
-    const allocator = gpa.allocator();
-
-    const result: u32 = try commonFactors(10, allocator);
-    try std.testing.expect(result == 2520);
+    try std.testing.expect(compute(10) == 2520);
 }
