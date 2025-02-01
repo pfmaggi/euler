d7c32c7562a9af1679fdbfa406870b98cbd8300e
Pietro F. Maggi <pfm@pietromaggi.com>
2025-02-01 19:30:12 +0100

    Refactor problem 4 in C++

--- cpp/p_0004.cc
+++ cpp/p_0004.cc
@@ -6,44 +6,36 @@
 #include <algorithm>
 #include <cmath>
 #include <iostream>
-#include <numeric>
 #include <string>
-#include <vector>
+#include <utility>
 
 bool is_palindrome(int num) {
     std::string s = std::to_string(num);
     return std::equal(s.begin(), s.begin() + s.size() / 2, s.rbegin());
 }
 
-int search_largest_palindrome(int limit) {
+std::pair<int, int> search_largest_palindrome(int limit) {
     int upper = std::pow(10, limit);
-    std::vector<int> range(upper);
-    std::iota(range.begin(), range.end(), 0);
 
-    std::vector<int> products;
-    for (int i : range) {
-        for (int j : range) {
-            products.push_back(i * j);
+    int largest_palindrome{0};
+    std::pair<int, int> factors;
+    for (int i{100}; i < upper; i++) {
+        for (int j{i}; j < upper; j++) {
+            int product{i * j};
+            if (is_palindrome(product) && (product > largest_palindrome)) {
+                largest_palindrome = product;
+                factors = std::pair<int, int>(i, j);
+            }
         }
     }
 
-    auto palindrome_products =
-        std::remove_if(products.begin(), products.end(),
-                       [](int a) { return !is_palindrome(a); });
-
-    products.erase(palindrome_products, products.end());
-
-    if (products.empty()) {
-        return 0;
-    }
-
-    return *std::max_element(products.begin(), products.end());
+    return factors;
 }
 
 int main() {
-    int result = search_largest_palindrome(3);
+    auto [i, j] = search_largest_palindrome(3);
     std::cout << "Problem 0004: The largest palindrome made from the product "
                  "of two 3-digit numbers is: "
-              << result << "\n";
+              << i << " x " << j << " = " << i * j << "\n";
     return 0;
 }
--- cpp/run.sh
+++ cpp/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Find and execute all p_XXXX files in numerical order
+# Find and execute all p_XXXX.out files in numerical order
 find . -maxdepth 1 -type f -name 'p_[0-9][0-9][0-9][0-9].out' -print0 | 
     sort -z -V | 
     while IFS= read -r -d $'\0' file; do
