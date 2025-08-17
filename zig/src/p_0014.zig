9c78a9c0faee0e2e45d1b4d829f8c7a42c9b21f1
Pietro F. Maggi <pfm@pietromaggi.com>
2025-08-17 10:02:31 +0200

    Refactor problem 14 in Zig

--- zig/src/p_0014.zig
+++ zig/src/p_0014.zig
@@ -8,13 +8,12 @@ const std = @import("std");
 fn compute_len(number: usize, array: []usize) usize {
     var value = number;
     var count: usize = 1;
-    while (value > 1) {
+    while (value > 1) : (count += 1) {
         if (value < number) {
             // if we have already seen this number we can
             // evaluate the final count for `number`
             return count + array[value] - 1;
         }
-        count += 1;
         if (value % 2 == 0) {
             // Current value is even
             // value = value / 2
@@ -29,9 +28,9 @@ fn compute_len(number: usize, array: []usize) usize {
 }
 
 fn euler_0014(comptime limit: usize) usize {
-    var array: [limit + 1]usize = std.mem.zeroes([limit + 1]usize);
+    var array: [limit + 1]usize = undefined;
 
-    var max: usize = 0;
+    var max: usize = 1;
     var result: usize = 1;
     for (1..limit) |n| {
         const count = compute_len(n, &array);
